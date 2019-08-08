035d6d18adf63a4ad7b91ac48b5e7f2b8e552049
Hernan Hernandez <hhernandez0924@gmail.com>
2019-08-08 19:29:17 -0400

    PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,9 +4,13 @@ import csv
 csvpath = os.path.join("..", "Pybank", "budget_data.csv")
 print(csvpath)
 
+
+
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    print(csvreader)
+    csvreader = next(csvreader)
+    months = []
 
-    for row in csvreader:
-        print(row)
+    for rows in csvreader:
+        months.append(rows[0])
+        print(months)
