2fc58ad7924fcbb057b52439176ef49d96a2ebac
Hernan Hernandez <hhernandez0924@gmail.com>
2019-08-10 13:05:10 -0400

    PyBankHW

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,6 +21,7 @@ with open(csvpath, newline="") as csvfile:
     budget_header = next(budget_reader)
 #created the list variable below for the profit/loss row.
     budget_num = []
+    diff = 0
 #created the for loop to append row 1 to budget_num
     for rows in budget_reader:
         budget_num.append(int(rows[1]))
@@ -31,10 +32,19 @@ with open(csvpath, newline="") as csvfile:
     net_total = sum (budget_num)
 #print successfull
     print(f'Total: $ {net_total}')
-    average_change = budget_num - budget_num
-    print(average_change)
-    
+    #diff = [budget_num[i] - budget_num[i+1] for i in range(len(budget_num)]
+    #print(diff)
 
+with open(csvpath, newline="") as csvfile:
+    budget_reader = csv.reader(csvfile, delimiter=",")
+    budget_header = next(budget_reader)
+    profit_loss = []
+    
+    for rows in budget_reader:
+        profit_loss.append(int(rows[1]))
+    diff = [profit_loss[i+1] - profit_loss[i] for i in range(len(profit_loss)-1)]
+    
+print(diff)
     
 
         
