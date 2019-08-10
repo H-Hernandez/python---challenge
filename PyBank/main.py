62ab70f74801bc8a1a9f4417eabfb87df0b9bc0f
Hernan Hernandez <hhernandez0924@gmail.com>
2019-08-09 23:23:15 -0400

    HW

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,10 +9,20 @@ print(csvpath)
 with open(csvpath, newline="") as csvfile:
     budget_reader = csv.reader(csvfile, delimiter=",")
     budget_header = next(budget_reader)
+    #created the list below for the dates, in order to generate the amount of months
     budget_date=[]
-
     for rows in budget_reader:
         budget_date.append(rows[0])
     
     print("Total months:", len(budget_date))
 
+with open(csvpath, newline="") as csvfile:
+    budget_reader = csv.reader(csvfile, delimiter=",")
+    budget_header = next(budget_reader)
+    #created the list below for the profit/loss calculations.
+    net_total = []
+    for i in budget_reader:
+        net_total.append(i[1])
+        print("Total sum:", sum(net_total))
+        
+    
